5436afd593f1dfa40ef4f31ad8b2639d2058c24f
aishwarya <ashp17star@gmail.com>
2026-06-11 12:49:51 +0530

    Custom Dialog Way 1

---
+++ app/src/main/java/com/example/a25_05_2026_dialogs_custom_demo/LoginDialog.java
@@ -0,0 +1,15 @@
+package com.example.a25_05_2026_dialogs_custom_demo;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+
+public class LoginDialog extends Dialog {
+
+    public LoginDialog(@NonNull Context context) {
+        super(context);
+    }
+}
